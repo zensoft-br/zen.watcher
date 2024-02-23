1692d1eaf264f24ea2b963f733ede2a538e15f8d
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-02-23 07:57:05 -0300

    Diversos

--- src/normalize.test.js
+++ src/normalize.test.js
@@ -9,4 +9,8 @@ test("", () => {
   expect(titleCase("15KM")).toBe("15km");
   expect(titleCase("de mim para você")).toBe("De Mim para Você");
   expect(titleCase("DE MIM PARA VOCÊ")).toBe("De Mim para Você");
+  expect(titleCase("um e dois")).toBe("Um e Dois");
+  expect(titleCase("UM E DOIS")).toBe("Um e Dois");
+  expect(titleCase("acme importaçao e exportaçao ltda.")).toBe("Acme Importaçao e Exportaçao Ltda.");
+  expect(titleCase("ACME IMPORTAÇAO E EXPORTAÇAO LTDA.")).toBe("Acme Importaçao e Exportaçao Ltda.");
 });
