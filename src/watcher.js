cf16153dacb954aa4d0f826763be5144f40fc223
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-02-27 09:40:47 -0300

    Diversos

--- src/watcher.js
+++ src/watcher.js
@@ -1,17 +1,20 @@
-// eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
   const zenRes = {
     statusCode: 200,
     body: {},
   };
 
-  /* When a productPacking is created, if no code is present and there is
-    a variant, we will auto assign a code */
+  // When a productPacking is created, if code is empty
+  // and variant is set, we will auto assign a code
   if (zenReq.body?.context?.event === "/catalog/product/productPackingCreate"
-    && (zenReq.body.context.tags ?? []).includes("before")) {
+    && (zenReq.body?.context?.tags ?? []).includes("before")) {
     const bean = zenReq.body.args.bean;
+
+    // Code is empty and variant is set
     if (!bean.code && bean.variant) {
       bean.code = `${bean.product.code }.${ bean.variant.code}`;
+
+      // Will return zenRes.body.args just if code is changed
       zenRes.body.args = zenReq.body.args;
     }
   }
