530d7863cfb48779bdd97a78165426ece08d7d88
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-05-30 21:49:28 -0300

    Diversos

--- src/logOpDeleteExpired.js
+++ src/logOpDeleteExpired.js
@@ -2,7 +2,7 @@ import * as Z from "@zensoftbr/zenerpclient";
 
 export async function logOpDeleteExpired(zenReq) {
   if (zenReq.path === "/system/audit/logOpDeleteExpired") {
-    const z = Z.createFromToken("http://localhost:8080/ZenErpWs", zenReq.body.context.token);
+    const z = Z.createFromToken(zenReq.body.context.tenant, zenReq.body.context.token);
 
     const auditService = new Z.api.system.audit.AuditService(z);
 
