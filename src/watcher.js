9e91573678bb445e1211254c6dbf51cf1c6a7d5a
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-05-31 07:58:54 -0300

    Diversos

--- src/watcher.js
+++ src/watcher.js
@@ -1,9 +1,15 @@
+import { lotCreate_setCodeSequence } from "./material/lot.js";
+
 // eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
   const zenRes = {
     statusCode: 200,
     body: {},
   };
+
+  if (zenReq.body?.context?.event === "/material/lotCreate" && zenReq.body?.context?.tags?.includes("before")) {
+    return await lotCreate_setCodeSequence(zenReq);
+  }
 
   return zenRes;
 }
