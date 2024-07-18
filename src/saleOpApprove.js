bba427a88d03f541cdd78b8cb679b760e1632986
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-07-18 17:29:37 -0300

    Diversos

--- src/saleOpApprove.js
+++ src/saleOpApprove.js
@@ -7,22 +7,25 @@ export async function saleOpApprove_cloneToFS(zenReq) {
   const saleService = new Z.api.sale.SaleService(z);
 
   const sale = await saleService.saleReadById(zenReq.body.args.id);
-  const items = await saleService.saleItemRead(`q=sale.id==${sale.id}`);
-  const payments = await saleService.salePaymentRead(`q=sale.id==${sale.id}`);
 
-  // TODO se passar o id o log registra neste id
-  items.forEach(e => {
-    e.id = undefined;
-    e.sale = undefined;
-  });
+  if (sale.company.id === 1002 && sale.saleProfile.code === "VendaCruzada") {
+    const items = await saleService.saleItemRead(`q=sale.id==${sale.id}`);
+    const payments = await saleService.salePaymentRead(`q=sale.id==${sale.id}`);
 
-  // TODO se passar o id o log registra neste id
-  payments.forEach(e => {
-    e.id = undefined;
-    e.sale = undefined;
-  });
+    // TODO se passar o id o log registra neste id
+    items.forEach(e => {
+      e.id = undefined;
+      e.sale = undefined;
+    });
 
-  if (sale.company.id === 1002) {
+    // TODO se passar o id o log registra neste id
+    payments.forEach(e => {
+      e.id = undefined;
+      e.sale = undefined;
+    });
+
+    const tags = (sale.tags ?? "").split(",").filter(e => e);
+
     let sale1 = {
       ...sale,
       // TODO se passar o id o log registra neste id
@@ -34,7 +37,7 @@ export async function saleOpApprove_cloneToFS(zenReq) {
         id: 1004,
       },
       code: `VB:${sale.id}`,
-      // tags: `VB:${sale.id}`,
+      tags: tags.concat("vendaCruzada").join(","),
     };
 
     // TODO está duplicando as formas de pagamento
