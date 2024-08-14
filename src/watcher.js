5e0d4cc1d9de04c6f4fe8adab75767fcc389802f
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-08-14 09:24:09 -0300

    Diversos

--- src/watcher.js
+++ src/watcher.js
@@ -1,6 +1,7 @@
 import { personCreate } from "./personCreate.js";
 import { instructionRequestOpCreateRegister } from "./instructionRequestOpCreateRegister.js";
 import { receivableOpApprove } from "./receivableOpApprove.js";
+import { saleOpApprove } from "./saleOpApprove.js";
 
 // eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
@@ -19,6 +20,10 @@ export async function watch(zenReq) {
 
   if (zenReq.body?.context?.event === "/financial/receivableOpApprove" && (zenReq.body?.context?.tags ?? []).includes("after")) {
     zenRes = await instructionRequestOpCreateRegister(zenReq);
+  }
+
+  if (zenReq.body?.context?.event === "/sale/saleOpApprove" && (zenReq.body?.context?.tags ?? []).includes("before")) {
+    zenRes = await saleOpApprove(zenReq);
   }
 
   return zenRes;
