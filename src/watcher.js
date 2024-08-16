a1e5d0fd989045ee076fc075b30cfd7b868c84d6
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-08-16 11:08:17 -0300

    Diversos

--- src/watcher.js
+++ src/watcher.js
@@ -1,21 +1,23 @@
-import { incomingList_createLot } from "./material/incomingList.js";
-import { lotCreate_setCodeSequence } from "./material/lot.js";
+import { incomingListItemCreate } from "./incomingListItemCreate.js";
 import { saleOpApprove_cloneToFS } from "./saleOpApprove.js";
 
-// eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
   const zenRes = {
     statusCode: 200,
     body: {},
   };
 
-  if (zenReq.body?.context?.event === "/material/incomingListCreate" && zenReq.body?.context?.tags?.includes("before")) {
-    return await incomingList_createLot(zenReq);
+  if (zenReq.body?.context?.event === "/material/incomingListItemCreate" && zenReq.body?.context?.tags?.includes("before")) {
+    return await incomingListItemCreate(zenReq);
   }
 
-  if (zenReq.body?.context?.event === "/material/lotCreate" && zenReq.body?.context?.tags?.includes("before")) {
-    return await lotCreate_setCodeSequence(zenReq);
+  if (zenReq.body?.context?.event === "/material/incomingListItemCreate" && zenReq.body?.context?.tags?.includes("after")) {
+    return await incomingListItemCreate(zenReq);
   }
+
+  // if (zenReq.body?.context?.event === "/material/lotCreate" && zenReq.body?.context?.tags?.includes("before")) {
+  //   return await lotCreate_setCodeSequence(zenReq);
+  // }
 
   if (zenReq.body?.context?.event === "/sale/saleOpApprove" && zenReq.body?.context?.tags?.includes("after")) {
     return await saleOpApprove_cloneToFS(zenReq);
