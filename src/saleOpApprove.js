471648ad744ce65c9f0ca39039da64a9c4f22af5
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2024-08-30 15:56:10 -0300

    Diversos

--- src/saleOpApprove.js
+++ src/saleOpApprove.js
@@ -9,9 +9,15 @@ export async function saleOpApprove(zenReq) {
 
   const sale = await saleService.saleReadById(zenReq.body.args.id);
 
+  // If today is a monday, consider past friday as the base date
+  var date = new Date();
+  if (date.getDay() === 1) {
+    date.setDate(date.getDate() - 2);
+  }
+
   // Find overdue receivables
   const receivableList = await financialService.receivableRead(
-    `q=person.id==${sale.person.id};status==APPROVED;dueDate<${new Date().toISOString().substring(0, 10)}`,
+    `q=person.id==${sale.person.id};status==APPROVED;dueDate<${date.toISOString().substring(0, 10)}`,
   );
   if (receivableList.length)
     throw new Error(`O cliente possui ${receivableList.length} título(s) vencido(s)\nSolicite aprovação de um operador com a permissão $act/aprovarNotasFiscaisDeSaidaComRestricoes`);
