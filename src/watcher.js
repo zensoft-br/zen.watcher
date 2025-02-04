85a156ff3ac5f88fa56c6a36ec1e46c84eec5563
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2025-02-04 05:51:09 -0300

    Diversos

--- src/watcher.js
+++ src/watcher.js
@@ -1,9 +1,15 @@
+import { saleOpCreate } from "./sale/saleOpCreate.js";
+
 // eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
   const zenRes = {
     statusCode: 200,
     body: {},
   };
+
+  if (zenReq.body.context.event === "/sale/saleOpCreate") {
+    return await saleOpCreate(zenReq);
+  }
 
   return zenRes;
 }
