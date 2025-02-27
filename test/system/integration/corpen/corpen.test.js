1d94a12cc00207a5e87da0fefad4fdeaf2455c5f
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2025-02-27 17:13:15 -0300

    Diversos

---
+++ test/system/integration/corpen/corpen.test.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { watch } from "../../../../src/watcher.js";
+
+watch({
+  method: "POST",
+  path: "/zenmail",
+  body: {
+    context: {
+      tenant: process.env.tenant,
+    },
+  },
+});
