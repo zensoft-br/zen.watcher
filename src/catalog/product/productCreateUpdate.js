72d03545a2c0b9e11d0b3480ad2aa2399007dfe3
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2025-05-16 12:30:15 -0300

    Diversos

--- src/catalog/product/productCreateUpdate.js
+++ src/catalog/product/productCreateUpdate.js
@@ -6,11 +6,17 @@ export async function productCreateUpdate(zenReq) {
   const bean = zenReq.body.args.bean;
 
   try {
+    if (!bean.category3) {
+      bean.category3 = { id: 2263 };
+    }
+
     bean.properties = bean.properties ?? {};
 
-    bean.properties.ps_largura_cm = round((bean.properties.textileWidth ?? 0) * 100, 4);
+    bean.properties.textileWidth = round((bean.properties.ps_largura_pol ?? 0) * 2.54 / 100, 4);
+    bean.properties.textileYield = round(1000 / (bean.properties.textileWidth * bean.properties.textileGramWeight), 2);
+
+    bean.properties.ps_largura_cm = round((bean.properties.textileWidth ?? 0) * 100, 2);
     bean.properties.ps_gramatura_ml = round((bean.properties.textileWidth ?? 0) * (bean.properties.textileGramWeight ?? 0), 4);
-    bean.properties.ps_largura_pol = round((bean.properties.textileWidth ?? 0) / 2.54 * 100, 4);
 
     if (bean.unit.code === "kg") {
       bean.netWeightKg = 1;
