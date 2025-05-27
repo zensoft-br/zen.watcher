9de5e2f0ea8eccbf3c1b3d46305f0fbe52f240ac
luiz-tempesta <luiz.tempesta@personalsoft.com.br>
2025-05-27 14:13:58 -0300

    Calculo automático de comissão

--- src/watcher.js
+++ src/watcher.js
@@ -1,3 +1,5 @@
+import { saleOpPrepare } from "./sale/saleOpPrepare.js";
+
 // eslint-disable-next-line no-unused-vars
 export async function watch(zenReq) {
   const zenRes = {
@@ -5,5 +7,16 @@ export async function watch(zenReq) {
     body: {},
   };
 
+  if (zenReq.body?.context?.event === "/sale/saleOpPrepare"
+    && (zenReq.body?.context?.tags ?? []).includes("before")) {
+    const result = await saleOpPrepare(zenReq);
+    if (result) {
+      zenRes = {
+        ...zenRes,
+        ...result,
+      };
+    }
+  }
+  
   return zenRes;
 }
